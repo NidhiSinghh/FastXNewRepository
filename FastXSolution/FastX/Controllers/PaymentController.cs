066085583fc589e860e35f4696c6911e3bf68020
NidhiSinghh <singhnidhik2002@gmail.com>
2024-03-02 16:24:06 +0530

    changes in the payment controller

--- FastXSolution/FastX/Controllers/PaymentController.cs
+++ FastXSolution/FastX/Controllers/PaymentController.cs
@@ -1,6 +1,7 @@
 ﻿using FastX.Exceptions;
 using FastX.Interfaces;
 using FastX.Models;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ namespace FastX.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("ReactPolicy")]
     public class PaymentController : Controller
     {
         private readonly IPaymentService _paymentService;
@@ -15,17 +17,41 @@ namespace FastX.Controllers
 
         public PaymentController(
             IPaymentService paymentService,
-            ILogger<PaymentController> logger) {
+            ILogger<PaymentController> logger)
+        {
             _paymentService = paymentService;
             _logger = logger;
         }
 
-        [HttpPost("create-payment/{bookingId}")]
-        public async Task<IActionResult> CreatePayment(int bookingId)
+        //[HttpPost("create-payment/{bookingId}")]
+        //public async Task<IActionResult> CreatePayment(int bookingId)
+        //{
+        //    try
+        //    {
+        //        await _paymentService.CreatePayment(bookingId);
+
+        //        return Ok("Payment created successfully");
+        //    }
+        //    catch (BookingNotFoundException ex)
+        //    {
+        //        _logger.LogError($"Booking not found. Error: {ex.Message}");
+        //        return NotFound("Booking not found");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError($"An unexpected error occurred. Error: {ex.Message}");
+        //        return StatusCode(500, "Internal Server Error");
+        //    }
+        //}
+
+
+
+        [HttpPost("create-payment")]
+        public async Task<IActionResult> CreatePayment(Payment payment)
         {
             try
             {
-                await _paymentService.CreatePayment(bookingId);
+                await _paymentService.CreatePayment(payment.BookingId);
 
                 return Ok("Payment created successfully");
             }
@@ -40,6 +66,14 @@ namespace FastX.Controllers
                 return StatusCode(500, "Internal Server Error");
             }
         }
+
+        //[HttpGet("RefundAmount")]
+        //public async Task<ActionResult<float>> FindRefundPrice(int userId, int bookingId)
+        //{
+        //    var refundAmount = await _paymentService.FindRefundPrice(userId, bookingId);
+        //    return Ok(refundAmount);
+        //}
+
 
 
 
